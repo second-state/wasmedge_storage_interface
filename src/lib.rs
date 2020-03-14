20271c2b8120ca9458a22690a839891544254c73
tpmccallum <mistermac2008@gmail.com>
2020-03-14 15:07:50 +1000

    V update

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,22 @@ extern crate rand;
 
 mod load {
 
-    //! Load i32
-    //! # Examples
-    //! ```
-    //! let my_previously_stored_i32_value: i32 = rust_storage_interface_library::load::loadi32(my_previously_saved_key: i64)
-    //! ```
+    /// Load i32
+    /// # Examples
+    /// ```
+    /// let my_previously_stored_i32_value: i32 = rust_storage_interface_library::load::loadi32(my_previously_saved_key: i64)
+    /// ```
     pub fn load_single_i32(_key: i64) -> i32 {
         // TODO - will require the syntax to interact with SecondState's other native library for SSVM which provides the database interaction
         // placeholder for now is 1
         1
     }
 
-    //! Load i64
-    //! # Examples
-    //! ```
-    //! let my_previously_stored_i64_value: i64 = rust_storage_interface_library::load::loadi64(my_previously_saved_key)
-    //! ```
+    /// Load i64
+    /// # Examples
+    /// ```
+    /// let my_previously_stored_i64_value: i64 = rust_storage_interface_library::load::loadi64(my_previously_saved_key)
+    /// ```
     pub fn load_single_i64(_key: i64) -> i64 {
         // TODO - will require the syntax to interact with SecondState's other native library for SSVM which provides the database interaction
         // placeholder for now is 1
@@ -30,9 +30,9 @@ mod load {
 mod store {
     use rand::Rng;
 
-    //! Create Random i64 Integer (which is positive i.e. non-negative)
-    //! Generates random i64 number and then converts it to an absolute value
-    //! Asserts that the value being returned is indeed absoulte i.e. is_positive
+    /// Create Random i64 Integer (which is positive i.e. non-negative)
+    /// Generates random i64 number and then converts it to an absolute value
+    /// Asserts that the value being returned is indeed absoulte i.e. is_positive
     fn create_random_i64() -> i64 {
         let mut rng = rand::thread_rng();
         let my_rand: i64 = rng.gen::<i64>().abs();
@@ -40,14 +40,14 @@ mod store {
         my_rand
     }
 
-    //! Store i32
-    //! Stores a single i32 value and returns a key which can be used to fetch the stored i32 value at a later date
-    //! # Examples
-    //! ```
-    //! my_i32_to_store: i32 = 88;
-    //! my_new_key: i64 = rust_storage_interface_library::store::storei32(my_i32_to_store)
-    //!
-    //! ```
+    /// Store i32
+    /// Stores a single i32 value and returns a key which can be used to fetch the stored i32 value at a later date
+    /// # Examples
+    /// ```
+    /// my_i32_to_store: i32 = 88;
+    /// my_new_key: i64 = rust_storage_interface_library::store::storei32(my_i32_to_store)
+    ///
+    /// ```
     pub fn store_single_i32(_value: i32) -> i64 {
         let new_key: i64 = create_random_i64();
         // TODO - will require the syntax to interact with SecondState's other native library for SSVM which provides the database interaction
@@ -55,14 +55,14 @@ mod store {
         new_key
     }
 
-    //! Store i64
-    //! Stores a single i64 value and returns a key which can be used to fetch the stored i64 value at a later date
-    //! # Examples
-    //! ```
-    //! my_i64_to_store: i64 = 88;
-    //! my_new_key: i64 = rust_storage_interface_library::store::storei64(my_i64_to_store)
-    //!
-    //! ```
+    /// Store i64
+    /// Stores a single i64 value and returns a key which can be used to fetch the stored i64 value at a later date
+    /// # Examples
+    /// ```
+    /// my_i64_to_store: i64 = 88;
+    /// my_new_key: i64 = rust_storage_interface_library::store::storei64(my_i64_to_store)
+    ///
+    /// ```
     pub fn store_single_i64(_value: i64) -> i64 {
         let new_key: i64 = create_random_i64();
         // TODO - will require the syntax to interact with SecondState's other native library for SSVM which provides the database interaction
