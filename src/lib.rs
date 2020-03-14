0bdedb58b4e327fcd79e3ad9302be4b015550a6f
tpmccallum <mistermac2008@gmail.com>
2020-03-14 14:50:38 +1000

    Examples

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate rand;
 mod load {
 
     /// Load i32
-    /// # Example
+    /// # Examples
     /// ```
     /// let my_previously_stored_i32_value: i32 = rust_storage_interface_library::load::loadi32(my_previously_saved_key: i64)
     ///```
@@ -15,7 +15,7 @@ mod load {
     }
 
     /// Load i64
-    /// # Example
+    /// # Examples
     /// ```
     /// let my_previously_stored_i64_value: i64 = rust_storage_interface_library::load::loadi64(my_previously_saved_key)
     ///```
@@ -41,7 +41,7 @@ mod store {
 
     /// Store i32
     /// Stores a single i32 value and returns a key which can be used to fetch the stored i32 value at a later date
-    /// # Example
+    /// # Examples
     /// ```
     /// my_i32_to_store: i32 = 88;
     /// my_new_key: i64 = rust_storage_interface_library::store::storei32(my_i32_to_store)
@@ -56,7 +56,7 @@ mod store {
 
     /// Store i64
     /// Stores a single i64 value and returns a key which can be used to fetch the stored i64 value at a later date
-    /// # Example
+    /// # Examples
     /// ```
     /// my_i64_to_store: i64 = 88;
     /// my_new_key: i64 = rust_storage_interface_library::store::storei64(my_i64_to_store)
